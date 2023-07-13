7676626e175796fae71a72ecddb945a4237676ba
MrQ1V <thepainmouth@gmail.com>
2023-07-13 15:07:17 +0300

    Изменение скорости движения корабля.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """Запуск событий на клавиатуре и мыши."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -32,9 +33,15 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Переместить корабль вправо.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (25,25,112)
+
+        # Настройки корабля
+        self.ship_speed = 1.5
