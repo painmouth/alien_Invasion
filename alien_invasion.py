93b8de4f376b0367f53af1075c5e1e77823b2ea2
MrQ1V <thepainmouth@gmail.com>
2023-07-15 12:24:47 +0300

    Создан ряд пришельцев. В зависимости от ширины 1 пришельца.

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,10 +79,21 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _create_fleet(self):
-        """Создание флота вторжения."""
-        # Создание пришельца.
+        """Создает флот пришельцев."""
+        # Создание пришельца и вычисление количества пришельцев в ряду.
+        # Интервал между соседними пришельцами равен ширине пришельца.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Создание первого ряда пришельцев.
+        for alien_number in range(number_aliens_x):
+            # Создание пришельца и размещение его в ряду.
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings():
         self.bg_color = (25,25,112)
 
         # Настройки корабля
-        self.ship_speed = 1.5
+        self.ship_speed = 1
 
         # Параметры стрельбы
         self.bullet_speed = 1
